0cd480d7a47a2187a97156535755de3d3dad474d
rana-mohamed10 <ranamuhamed924@gmail.com>
2023-11-03 04:43:13 +0200

    Browse category

--- lib/ApiManager/apiManager.dart
+++ lib/ApiManager/apiManager.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:movies/BrowseResponse/CategoryResponse.dart';
 import 'package:movies/SearchResponse/SearchResponse.dart';
 
 
 class apiManager{
-
 
   static Future<SearchResponse> get(String search) async {
     var url = Uri.https('api.themoviedb.org', '/3/search/movie', {
@@ -20,4 +20,17 @@ class apiManager{
     var searchResponse = SearchResponse.fromJson(json);
     return searchResponse;
   }
+
+  static Future<CategoryResponse> getCategory() async {
+    var url = Uri.https('api.themoviedb.org', '/3/genre/movie/list', {
+      'api_key': '91d26df9fb64973d39a9e876ce58da73'
+    });
+
+    var response = await http.get(url);
+    var json = jsonDecode(response.body);
+    var categoryResponse = CategoryResponse.fromJson(json);
+    return categoryResponse;
+  }
+
+
 }
